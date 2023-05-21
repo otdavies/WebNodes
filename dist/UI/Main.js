3f4c21e4bb454c9edb1630be50ed6cadc335f791
otdavies <oliver@psyfer.io>
2023-05-21 00:43:32 -0700

    Keep Main.ts separated from the UI

--- dist/UI/Main.js
+++
@@ -1,9 +0,0 @@
-// --- IMPORTS ---
-import { InspectorPanel } from './InspectorPanel.js';
-import { NodePanel } from './NodePanel.js';
-import { OptionPanel } from './OptionPanel.js';
-import { cogIcon, nodePanel, optionPanel } from './Shared.js';
-// Initialize the app
-let inspector = new InspectorPanel();
-let nodeGraph = new NodePanel(nodePanel, inspector);
-let options = new OptionPanel(cogIcon, optionPanel);
--- ts/Main.ts
+++ ts/Main.ts
@@ -1,8 +1,8 @@
 // --- IMPORTS ---
-import { InspectorPanel } from './InspectorPanel.js';
-import { NodePanel } from './NodePanel.js';
-import { OptionPanel } from './OptionPanel.js';
-import { cogIcon, nodePanel, optionPanel } from './Shared.js';
+import { InspectorPanel } from './UI/InspectorPanel.js';
+import { NodePanel } from './UI/NodePanel.js';
+import { OptionPanel } from './UI/OptionPanel.js';
+import { cogIcon, nodePanel, optionPanel } from './UI/Shared.js';
 
 
 // Initialize the app
